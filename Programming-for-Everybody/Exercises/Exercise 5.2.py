697b9dd63bb92e3be20214fc71d6a95116211cad
dtammam <dean@tamm.am>
2021-08-20 13:31:46 -0400

    I got my first cert!
    Finished the final exercises, and got my cert!
    
    coursera.org/verif y/SB7CF2MHDR3U
    
    Four more to go, the learning never continues.
    Oddly enough this code continually shows syntax problems. I've tested and confirmed its' good, even referenced with a friend after-the-fact... strange.

--- Programming-for-Everybody/Exercises/Exercise 5.2.py
+++ Programming-for-Everybody/Exercises/Exercise 5.2.py
@@ -1,23 +1,28 @@
-def scriptContext() :
-    """
-    5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. 
-    Once 'done' is entered, print out the largest and smallest of the numbers. 
-    If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. 
-    Enter 7, 2, bob, 10, and 4 and match the output below.
-    """
+# def scriptContext() :
+#     """
+#     5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'.
+#     Once 'done' is entered, print out the largest and smallest of the numbers.
+#     If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number.
+#     Enter 7, 2, bob, 10, and 4 and match the output below.
+#     """
 
-largest = None
+largest = -1
 smallest = None
 
 while True:
-    num = input("Enter a number: ")
-    
-    if 
-    
-    if num == "done":
+    string_input = input("Enter a number: ")
+    if string_input == "done":
         break
-    
-
-    print(num)
-
-print("Maximum", largest)
+    try:
+        validinput = float(string_input)
+    except:
+        print("Invalid input")
+        continue
+    if validinput > largest:
+        largest = validinput
+    if smallest is None:
+        smallest = validinput
+    elif validinput < smallest:
+        smallest = validinput
+print("Maximum is", int(largest))
+print("Minimum is", int(smallest))
