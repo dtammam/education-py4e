a34533d2eb48c141c7b1704d3a00ab0c2f6eab40
dtammam <dean@tamm.am>
2021-10-16 23:54:56 -0400

    Brain hurting with scraping!
    Learned a lot about tags and got through one assignment, second is squeezing my brain badly. Will revisit tomorrow!

---
+++ Programming-for-Everybody/Concepts/12.5 Reference for BeautifulSoup.py
@@ -0,0 +1,21 @@
+'''
+Reference for BeautifulSoup
+    - The TCP/IP gives us pipes/sockets between applications
+	- We designed application protocols to make use of these pipes
+	- HyperText Transfer Protocol (HTTP) is a simple yet powerful protocol
+    - Python has good support for sockets, HTTP, and HTML parsing
+'''
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+# Try this URL
+# http://www.dr-chuck.com/page1.htm
+
+url = input('http://www.dr-chuck.com/page1.htm')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
