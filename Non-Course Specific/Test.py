bef5d3195c3b450f6f9a3e8caac32d3de86ab076
dtammam <dean@tamm.am>
2021-12-20 22:12:46 -0500

    Obtained cert 5 of 5!
    Kind of anticlimactic - just a final test. But, I have to continue now going through the main projects. More coming soon!

---
+++ Non-Course Specific/Test.py
@@ -0,0 +1,18 @@
+# def bmi(weight, height):
+#     h2 = height * height
+#     fullindex = weight / h2
+    
+#     if fullindex <= 18.5:
+#         return "Underweight"
+#     elif fullindex <= 25:
+#         return "Normal"
+#     elif fullindex <= 30:
+#         return "Overweight"
+
+#     elif fullindex > 30:
+#         return "Obese"
+
+total = 0
+for abc in range(5):
+    total = total + abc
+print(total)
